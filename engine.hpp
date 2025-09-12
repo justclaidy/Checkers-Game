2a4b5c2756d50c42716a1be555fca0372715f145
justclaidy <121346501+justclaidy@users.noreply.github.com>
2025-09-12 11:01:49 +0300

    Create engine.hpp

---
+++ engine.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "board.hpp"
+#include "player.hpp"
+#include "result.hpp"
+
+class GameEngine {
+public:
+    GameEngine();
+    void start();
+    GameResult getGameState() const;
+
+private:
+    Board board;
+    Player player1;
+    Player player2;
+    Player* currentPlayer;
+    GameResult result;
+
+    void switchPlayer();
+    void updateResult(); 
+};
