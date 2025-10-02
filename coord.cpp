02ae27cb310aa89f8253d7cfaf98fe542bdf15e6
justclaidy <thelupis3@gmail.com>
2025-10-03 00:21:29 +0300

    lab02: implementare clase Checkers + Makefile

--- coord.cpp
+++ coord.cpp
@@ -1,9 +1,5 @@
-#pragma once
+#include "coord.hpp"
 
-struct Coord {
-    int x;
-    int y;
+Coord::Coord() : x(0), y(0) {}
 
-    Coord();                
-    Coord(int _x, int _y);   
-};
+Coord::Coord(int _x, int _y) : x(_x), y(_y) {}
