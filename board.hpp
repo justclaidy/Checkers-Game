44623fe263a3131cf7c5099f7a731b369fc6e87c
justclaidy <121346501+justclaidy@users.noreply.github.com>
2025-11-07 12:46:21 +0200

    Add addPiece method to Board struct

--- board.hpp
+++ board.hpp
@@ -5,10 +5,11 @@
 #include <vector>
 struct Board {
     int width, height;
-    std::vector<std::pair<Piece, Coord>> pieces; 
+    std::vector<std::pair<Piece, Coord>> pieces;
     Board(int w=8, int h=8);
     Board(const Board& other);
     void init();
+    void addPiece(const Piece& piece, const Coord& pos); 
     Board& operator=(const Board& other);
     bool operator==(const Board& other) const;
 };
