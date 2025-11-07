a58988d1f1d77294587d17d69bdc3b15891777d5
justclaidy <121346501+justclaidy@users.noreply.github.com>
2025-11-07 12:39:59 +0200

    Refactor Board to use pairs of Piece and Coord

--- board.hpp
+++ board.hpp
@@ -1,20 +1,16 @@
 #pragma once
 #include "piece.hpp"
+#include "coord.hpp"
 #include <iostream>
 #include <vector>
-
-struct Piece { char s; int x, y; };
-
 struct Board {
     int width, height;
-    std::vector<Piece> pieces;
-
+    std::vector<std::pair<Piece, Coord>> pieces; 
     Board(int w=8, int h=8);
     Board(const Board& other);
     void init();
     Board& operator=(const Board& other);
     bool operator==(const Board& other) const;
 };
-
 std::istream& operator>>(std::istream& in, Board& b);
 std::ostream& operator<<(std::ostream& out, const Board& b);
