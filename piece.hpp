58182de0347a53292c53dd00ae6f488e32f530ae
justclaidy <121346501+justclaidy@users.noreply.github.com>
2025-11-07 12:40:38 +0200

    Fix formatting and add missing newlines in piece.hpp

--- piece.hpp
+++ piece.hpp
@@ -1,21 +1,16 @@
 #pragma once
 #include <iostream>
 #include <string>
-
 enum PieceColor { WHITE, BLACK };
 enum PieceType { MAN, QUEEN };
-
 struct Piece {
     PieceColor color;
     PieceType type;
-
     Piece(PieceColor c = WHITE, PieceType t = MAN);
     Piece(const Piece& other);
-
     Piece& operator=(const Piece& other);
     bool operator==(const Piece& other) const;
-    std::string ToString() const;
+    std::string ToString() const; 
 };
-
 std::istream& operator>>(std::istream& in, Piece& p);
 std::ostream& operator<<(std::ostream& out, const Piece& p);
